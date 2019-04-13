f03569eaeb3a22cdda90b731fc7e2d5fa95ba19b
Kevin Ngoh <kngoh@student.unimelb.edu.au>
2019-04-13 22:37:01 +1000

    WebPage Added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var http = require ('http');
+var fs = require ('fs');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -9,6 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require('./routes/routes.js');
 app.use('/', routes);
 
+var app = http.createServer(function(req,res) {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/homepage.html', 'utf8');
+    myReadStream.pipe(res);
+});
+
 app.listen(3000, function(req, res) {
-    console.log('Express listening on port ${PORT}');
+    console.log(`Express listening on port ${PORT}`);
 });
