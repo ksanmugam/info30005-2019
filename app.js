221b9d3759326f5db64d5506807d0d900029a7ff
Kevin Ngoh <kngoh@student.unimelb.edu.au>
2019-04-14 00:27:04 +1000

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var http = require ('http');
-var fs = require ('fs');
+// var http = require ('http');
+// var fs = require ('fs');
 
-const PORT = process.env.PORT || 3001;
+const PORT = (process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,12 +16,12 @@ app.use('/', routes);
 //     console.log('Express listening on port 3000');
 // });
 
-var app = http.createServer(function(req,res) {
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/homepage.html', 'utf8');
-    myReadStream.pipe(res);
-});
+// var app = http.createServer(function(req,res) {
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var myReadStream = fs.createReadStream(__dirname + '/homepage.html', 'utf8');
+//     myReadStream.pipe(res);
+// });
 
 app.listen(PORT, function() {
     console.log(`Express listening on port ${PORT}`);
@@ -30,4 +30,4 @@ app.listen(PORT, function() {
 // server.listen(3000, '127.0.0.1');
 // console.log ('yo guess what, we listening on port 3000');
 
-module.exports = app;
+// module.exports = app;
