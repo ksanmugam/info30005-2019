b2414f356827c3ff55e083edeace6d7978e1161b
cwilson4 <cwilson4@student.unimelb.edu.au>
2019-04-27 23:09:44 +1000

    Added db.js dependency

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Database setup
+require('./models/db.js');
+
 var routes = require('./routes/routes.js');
 app.use('/', routes);
 
