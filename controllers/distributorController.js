d2e2b3c63e848573f79c2fd39e6e3f67fff2c799
ksanmugam <kirentheren@gmail.com>
2019-04-28 11:30:18 +1000

    fixed distributor controller

--- controllers/distributorController.js
+++ controllers/distributorController.js
@@ -37,30 +37,57 @@ var findAllDistributors = function(req, res) {
 
 //returns distributor with specified cuisine
 var findByCuisine = function(req, res) {
-    for (var i = 0; i < distributors.length; i++) {
-        if (distributors[i].cuisine == req.params.cuisine) {
-            res.send(distributors[i]);
-        }
-    }
+	var cuisines = req.params.cuisine;
+    Distributor.find({cuisine:cuisines}, function(err,distributors){
+    	if(!err){
+    		res.send(distributors);
+    	} else {
+    		res.sendStatus(500);
+    	}
+    });
 };
+//	for (var i = 0; i < distributors.length; i++) {
+//        if (distributors[i].cuisine == req.params.cuisine) {
+//            res.send(distributors[i]);
+//        }
+//    }
+//};
 
 //returns distributor with specific food on offer
 var findByFoodName = function(req, res) {
-    for (var i = 0; i < distributors.length; i++) {
-        if (distributors[i].food_name == req.params.food_name) {
-            res.send(distributors[i]);
-        }
-    }
+	var foodname = req.params.food_name;
+	Distributor.find({food_name:foodname}, function(err,distributors){
+		if(!err){
+			res.send(distributors);
+		} else {
+			res.sendStatus(500);
+		}
+	});
 };
+//    for (var i = 0; i < distributors.length; i++) {
+//        if (distributors[i].food_name == req.params.food_name) {
+//            res.send(distributors[i]);
+//        }
+//    }
+//};
 
 //returns all ditributors whose food contains the specified ingredient
 var findByIngredient = function(req, res) {
-    for (var i = 0; i < distributors.length; i++) {
-        if((distributors[i].ingredients).includes(req.params.ingredients)) {
-            res.send(distributors[i]);
-        }
-    }
-}
+	var ingredient = req.params.ingredients;
+	Distributor.find({ingredients:ingredient}, function(err,distributors){
+		if(!err){
+			res.send(distributors);
+		} else {
+			res.sendStatus(500);
+		}
+	});
+};
+//    for (var i = 0; i < distributors.length; i++) {
+//        if((distributors[i].ingredients).includes(req.params.ingredients)) {
+//            res.send(distributors[i]);
+//        }
+//    }
+//}
 
 
 module.exports.createDistributor = createDistributor;
